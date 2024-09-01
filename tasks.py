6a014ca297260765d63cf1fdf5b8d3c55bc4f7b2
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-09-01 19:11:44 -0300

    feat: add logging ingo

--- tasks.py
+++ tasks.py
@@ -4,6 +4,7 @@ from robocorp.tasks import task
 from aljazeera_service import AljazeeraService
 from browser import Browser
 from exporter import ExporterService
+from locallogger import logger
 
 
 @task
@@ -15,6 +16,7 @@ def search_news():
         results = service.execute(
             item.payload["query"], item.payload["order_by"], item.payload["months"]
         )
+        logger.info(f"Found {len(results)} news")
         service.browser.driver_quit()
         exportation = ExporterService(results)
         exportation.execute()
